bf3a27f87cad7b4c74bdee1339e08c4ad73dd36c
joe/Overlay-Asmaro-Joe <shahid.asmar2023@gmail.com>
2026-08-27 00:47:20 +0300

    Fix Header path issue

---
+++ src/components/Header.tsx
@@ -0,0 +1,1 @@
+import React from "react"; export const Header = () => null; export default Header;
---
+++ src/lib/defaultData.ts
@@ -0,0 +1,12 @@
+export const initialStoreData = {
+  config: {
+    storeName: 'Overlay Asmaro',
+    whatsappNumber: '',
+    wishMoneyNumber: '',
+    adminPasswordHash: '2255',
+    logoColor: '#f43f5e',
+    logoUrl: '',
+  },
+  categories: [],
+  items: [],
+};
